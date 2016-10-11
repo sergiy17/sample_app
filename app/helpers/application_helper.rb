190742e979c09d2c5b071804c0a62e738b3e6851
Sergiy17 <serparkhuts@gmail.com>
2016-10-12 00:35:04 +0300

    fixing test for home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title)
+		@basic_title = "Ruby on Rails Tutorial Sample App"
+		if page_title
+			return page_title + " | " + @basic_title
+		else
+			return @basic_titles
+		end
+	end
 end
